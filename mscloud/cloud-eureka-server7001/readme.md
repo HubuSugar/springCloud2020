5d66e6b02590d6dccb47295f46fe1fb1bd3d07d6
HubuSugar <531982033@qq.com>
2020-05-31 01:57:04 +0800

    添加eureka服务端、EnableEurekaServer注解

---
+++ mscloud/cloud-eureka-server7001/readme.md
@@ -0,0 +1,3 @@
+1.引入eureka的服务端spring-cloud-starter-netflix-eureka-server
+2.在主启动类上加上@EnableEurekaServer注解
+3.访问localhost:7001
---
+++ mscloud/cloud-eureka-server7001/src/main/java/edu/hubu/springcloud/EurekaMain7001.java
@@ -0,0 +1,17 @@
+package edu.hubu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * created by Sugar  2020/5/31 1:48
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class,args);
+    }
+}
