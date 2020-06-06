d688ea535697673c739beb0f22a94c24191ba33c
HubuSugar <531982033@qq.com>
2020-06-06 23:12:11 +0800

    配置Eureka集群

---
+++ mscloud/cloud-eureka-server7002/src/main/java/edu/hubu/springcloud7002/EurekaMain7002.java
@@ -0,0 +1,17 @@
+package edu.hubu.springcloud7002;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * created by Sugar  2020/6/6 23:04
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class);
+    }
+}
