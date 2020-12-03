4344544fe64e901f956c53316e23a8f327141ae7
HubuSugar <531982033@qq.com>
2020-12-03 22:52:43 +0800

    广播通知动态刷新3355和3366

---
+++ mscloud/cloud-config-client3366/src/main/java/edu/hubu/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,17 @@
+package edu.hubu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * created by Sugar  2020/12/3 22:34
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class,args);
+    }
+}
---
+++ mscloud/cloud-config-client3366/src/main/java/edu/hubu/springcloud/controller/ClientController.java
@@ -0,0 +1,27 @@
+package edu.hubu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * created by Sugar  2020/12/3 22:37
+ */
+@RestController
+@RefreshScope
+public class ClientController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String configInfo()
+    {
+        return "serverPort: "+serverPort+"\t\n\n configInfo: "+configInfo;
+    }
+
+}
